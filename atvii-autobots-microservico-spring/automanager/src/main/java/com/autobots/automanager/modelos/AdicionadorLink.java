54efcccc08fd77cc08858c72964efa28f9bb9e7a
ZeroPirata <gabriel0gsbn@gmail.com>
2022-09-27 16:01:26 -0300

    Inicio da atividade 3

--- atvii-autobots-microservico-spring/automanager/src/main/java/com/autobots/automanager/modelos/AdicionadorLink.java
+++
@@ -1,9 +0,0 @@
-package com.autobots.automanager.modelos;
-
-import java.util.List;
-
-
-public interface AdicionadorLink<T> {
-	public void adicionarLink(List<T> lista);
-	public void adicionarLink(T objeto);
-}
